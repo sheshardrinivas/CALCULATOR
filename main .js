5e96231f732904a35b47f06f0c1035baf667a805
sheshardrinivas <127858531+sheshardrinivas@users.noreply.github.com>
2023-03-23 19:23:47 +0530

    Add files via upload

---
+++ main .js
@@ -0,0 +1,26 @@
+function mul() {
+    var result =
+    Number(document.getElementById("firstno").value) *
+    Number(document.getElementById("2no").value);
+    document.getElementById("result").innerHTML=result;
+    }
+    function add() {
+    var add  =
+    Number(document.getElementById("firstno").value) +
+    Number(document.getElementById("2no").value);
+    document.getElementById("result").innerHTML=add;
+    }
+    function sub() {
+    var sub =
+    Number(document.getElementById("firstno").value) -
+    Number(document.getElementById("2no").value);
+    document.getElementById("result").innerHTML=sub;
+    }
+    function div() {
+    var div =
+    Number(document.getElementById("firstno").value) /
+    Number(document.getElementById("2no").value);
+    document.getElementById("result").innerHTML=div;
+    }
+    
+    
